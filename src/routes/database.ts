8c15d08965651f749040f62f8a488197a46fb340
boybook <fangyizhou1997@qq.com>
2022-03-06 23:28:40 +0800

    token check

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -3,6 +3,19 @@ import controller from '../controllers/database';
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    if (!req.header("notion_token")) {
+        return res.status(400).json({
+            object: 'error',
+            status: 400,
+            code: 'bad request',
+            message: 'Head missing (notion_token)',
+            header: req.headers
+        });
+    }
+    next();
+});
+
 router.post('/:database/query', controller.query);
 
 export default router;
--- src/routes/node.ts
+++ src/routes/node.ts
@@ -3,6 +3,19 @@ import controller from '../controllers/node';
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    if (!req.header("figma_token") || !req.header("notion_token") || !req.header("notion_database")) {
+        return res.status(400).json({
+            object: 'error',
+            status: 400,
+            code: 'bad request',
+            message: 'Head missing (figma_token, notion_token, notion_database)',
+            header: req.headers
+        });
+    }
+    next();
+});
+
 router.get('/get/:file/:node', controller.getNode);
 router.post('/push/:file/:node', controller.pushNode);
 router.delete('/delete/:file/:node', controller.deleteNode);
